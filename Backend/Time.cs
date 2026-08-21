d9551aa09c219f051764c12ec73fcb57f1fc6525
jmgiraldod14 <jmgiraldod@gmail.com>
2026-08-20 20:12:33 -0500

    Constructors, ToString and tests

---
+++ Backend/Time.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Backend;
+
+public class Time
+{
+    //Fields
+    private int _hour;
+    private int _minute;
+    private int _second;
+    private int _millisecond;
+   
+
+
+    //Constructors
+    public Time()
+    {
+        _hour = 0;
+        _minute = 0;
+        _second = 0;
+        _millisecond = 0;
+        
+    }
+
+    public Time(int hour)
+    {
+        _hour = hour;
+        _minute = 0;
+        _second = 0;
+        _millisecond = 0;
+      
+    }
+
+    public Time(int hour, int minute)
+    {
+        _hour = hour;
+        _minute = minute;
+        _second = 0;
+        _millisecond = 0;
+    }
+
+    public Time(int hour, int minute, int second)
+    {
+        _hour = hour;
+        _minute = minute;
+        _second = second;
+        _millisecond = 0;
+    }
+
+    public Time(int hour, int minute, int second, int millisecond)
+    {
+        _hour= hour;
+        _minute = minute;
+        _second = second;
+        _millisecond =millisecond;
+    }
+
+    //Properties
+
+
+
+    //Public Methods
+
+    public override string ToString()
+    {
+        string period;
+
+        if (_hour < 12)
+        {
+            period = "AM"; 
+        }
+        else
+        {
+            period = "PM";
+        }
+        return $"{_hour:D2}:{_minute:D2}:{_second:D2}.{_millisecond:D3} {period}";
+    }
+
+
+
+
+}
---
+++ Frontend/Program.cs
@@ -0,0 +1,11 @@
+﻿using Backend;
+
+var time1 = new Time(); //tiempo cero
+var time2 = new Time(15); //horas
+var time3 = new Time(8, 32, 54, 214);
+var time4 = new Time(23, 2, 7);
+
+Console.WriteLine(time1.ToString());
+Console.WriteLine(time2.ToString());
+Console.WriteLine(time3.ToString());
+Console.WriteLine(time4.ToString());
